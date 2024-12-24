c55c4531141fd46294c733f771e86a227d04067b
karabinn <krabin.dev@gmail.com>
2024-12-24 14:06:24 +0100

    Etape 2 : ajout de la fonction insertWorkHtml

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,27 +1,34 @@
-// ETAPE 1 URL de l'image
-const imageUrl = "http://localhost:5678/images/abajour-tahina1651286843956.png";
-
-// Création des éléments nécessaires
-const figure = document.createElement("figure");
-const img = document.createElement("img");
-const figcaption = document.createElement("figcaption");
-
-// Configuration des attributs et du contenu
-img.src = imageUrl;
-img.alt = "Abat-jour Tahina"; // Texte alternatif pour l'image
-figcaption.textContent = "Abat-jour Etape 1";
-
-// Assemblage de la figure
-figure.appendChild(img);
-figure.appendChild(figcaption);
-
-
-// Récupération de l'élément parent où insérer la figure
-const gallery = document.querySelector(".gallery");
-
-// Ajout de la figure dans la galerie
-if (gallery) {
-  gallery.appendChild(figure);
-} else {
-  console.error("L'élément .gallery est introuvable dans le DOM.");
-}
+//Etape 2
+// Définition de la fonction insertWorkHtml
+function insertWorkHtml(imageUrl, legend, alt) {
+    // Création des éléments nécessaires
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const figcaption = document.createElement("figcaption");
+  
+    // Configuration des attributs et du contenu
+    img.src = imageUrl;    // URL dynamique
+    img.alt = alt;         // Texte alternatif dynamique
+    figcaption.textContent = legend; // Légende dynamique
+  
+    // Assemblage de la figure
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+  
+    // Récupération de l'élément parent où insérer la figure
+    const gallery = document.querySelector(".gallery");
+  
+    // Ajout de la figure dans la galerie si l'élément existe, sinon msg d'erreur
+    if (gallery) {
+      gallery.appendChild(figure);
+    } else {
+      console.error("L'élément .gallery est introuvable dans le DOM.");
+    }
+  }
+  
+  // Test de la fonction
+  insertWorkHtml(
+    "http://localhost:5678/images/abajour-tahina1651286843956.png", 
+    "Abat jour Etape 2", 
+    "Abat jour Etape 2",
+  );
